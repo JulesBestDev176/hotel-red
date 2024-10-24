eb15e54fe23db420e99599239ce0d0df30591927
JulesBestDev176 <souleymanefall176@gmail.com>
2024-10-24 15:36:53 +0000

    install et configure cors

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
+import cors from "cors"; // Assurez-vous d'importer cors
 import { connectDB } from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import deviseRoutes from "./routes/deviseRoutes.js";
@@ -10,6 +11,16 @@ import hotelRoutes from "./routes/hotelRoutes.js";
 dotenv.config();
 
 const app = express();
+
+// Options de configuration CORS
+const corsOptions = {
+  origin: "https://hotel-red-1.onrender.com", // Autoriser cette origine
+  methods: ["GET", "POST", "PUT", "DELETE"], // Méthodes autorisées
+  credentials: true, // Si vous utilisez des cookies
+};
+
+// Utilisation de CORS
+app.use(cors(corsOptions));
 
 app.use(helmet());
 app.use(cookieParser());
@@ -25,7 +36,14 @@ app.use("/api", userRoutes);
 app.use("/api", deviseRoutes);
 app.use("/api", hotelRoutes);
 
-app.listen(5000, () => {
+// Lancer le serveur
+app.listen(process.env.PORT || 5000, () => {
   connectDB();
-  console.log("Server started at http://localhost:5000");
+  console.log(
+    `Server started at ${
+      process.env.PORT
+        ? "http://localhost:" + process.env.PORT
+        : "http://localhost:5000"
+    }`
+  );
 });
