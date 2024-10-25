20cd76c0b0ddd59f6e0ca3c5ab8db0e720146323
JulesBestDev176 <souleymanefall176@gmail.com>
2024-10-25 22:41:18 +0000

    update imageName, et chemin src de recuperation d'image

--- backend/server.js
+++ backend/server.js
@@ -57,7 +57,7 @@ app.use("/api", hotelRoutes);
 // Lancer le serveur
 app.listen(process.env.PORT || 5000, () => {
   connectDB();
-  console.log("path : " + path.join(__dirname, "../public/assets"));
+  console.log("path : " + path.join(__dirname, "public/assets"));
   console.log(
     `Server started at ${
       process.env.PORT
