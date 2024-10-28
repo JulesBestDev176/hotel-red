205f924dfe00208da4045599342a513f4d7512bf
JulesBestDev176 <souleymanefall176@gmail.com>
2024-10-28 08:43:16 +0000

    debuggage image

--- backend/controllers/hotelController.js
+++ backend/controllers/hotelController.js
@@ -40,6 +40,7 @@ export const createHotel = async (req, res) => {
     res
       .status(201)
       .json({ success: true, data: { ...newHotel._doc, image: imageUrl } });
+    console.log("img url : ", imageUrl);
   } catch (error) {
     console.log(`Erreur lors de la création de l'hôtel: ${error.message}`);
     res.status(500).json({ success: false, message: "Server error" });
--- backend/middlewares/upload.js
+++ backend/middlewares/upload.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     const uploadPath = path.join(__dirname, "../public/assets/images/hotel");
-
+    console.log("uploadPath : ", uploadPath);
     // Vérifiez si le répertoire existe, sinon créez-le
     fs.mkdir(uploadPath, { recursive: true }, (err) => {
       if (err) {
